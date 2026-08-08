0b1b45bbc456dfd7741e82cfdde3469885361bd9
Nils <nilsscharein@yahoo.de>
2026-08-08 22:59:56 +0200

    ci(backend): add jenkins pipeline and ktlint

--- backend/src/main/kotlin/dev/nils/backend/controller/HelloWorldController.kt
+++ backend/src/main/kotlin/dev/nils/backend/controller/HelloWorldController.kt
@@ -1,14 +1,10 @@
 package dev.nils.backend.controller
 
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloWorldController {
-
     @GetMapping("/hello")
-    fun helloWorld() : String {
-        return "Hello World!"
-    }
+    fun helloWorld(): String = "Hello World!"
 }
