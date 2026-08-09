c6a5ac9ffb5ff176915451d969b2c4a3d5fac871
Nils <nilsscharein@yahoo.de>
2026-08-09 15:23:33 +0200

    ci: pipeline fail test

--- backend/src/test/kotlin/dev/nils/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/dev/nils/backend/BackendApplicationTests.kt
@@ -1,11 +1,13 @@
 package dev.nils.backend
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BackendApplicationTests {
     @Test
     fun contextLoads() {
+        fail("Not implemented yet");
     }
 }
