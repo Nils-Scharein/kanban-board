b411d33a5b84658c0ddc205ec1c5314a8fc29745
Nils <nilsscharein@yahoo.de>
2026-08-23 13:22:55 +0200

    feat(frontend): add openApi generation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { heyApiPlugin } from '@hey-api/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    heyApiPlugin({
+      config: {
+        input: 'http://localhost:8080/v3/api-docs',
+        output: 'src/__generated_client__',
+      },
+    }),
+  ],
 })
